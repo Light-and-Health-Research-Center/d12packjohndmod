368d5ead2c9475a5eedb876a8c9012564d134f1f
Geoffrey Jones <jonesg5@rpi.edu>
2017-01-27 16:52:38 -0500

    fixed rgb2cla

--- +d12pack/@DaysimeterData/rgb2cla.m
+++ +d12pack/@DaysimeterData/rgb2cla.m
@@ -10,11 +10,11 @@ if obj.SerialNumber < 366
     Vm = [ 0.381876  0.642883 0.067544];
     Vp = [ 0.004458  0.360213 0.189536];
 elseif obj.SerialNumber >= 366 && obj.SerialNumber <= 415
-    C = [0.613631	3.385647	0.235952	2.310402];
-    M = [-0.001684	0.164230	0.265890];
-    Sm = [-0.005284	-0.015436	0.243373];
-    Vm = [0.380969	0.647984	0.057097];
-    Vp = [0.001286	0.361920	0.191571];
+    C  = [ 0.613631  3.385647 0.235952 2.310402];
+    M  = [-0.001684  0.164230 0.265890];
+    Sm = [-0.005284 -0.015436 0.243373];
+    Vm = [ 0.380969  0.647984 0.057097];
+    Vp = [ 0.001286  0.361920 0.191571];
 else
     error(['Serial number: ',num2str(obj.SerialNumber),' is not supported']);
 end
@@ -25,6 +25,12 @@ Scone      = sum(bsxfun(@times, Sm, RGB), 2);
 Vmaclamda  = sum(bsxfun(@times, Vm, RGB), 2);
 Melanopsin = sum(bsxfun(@times, M,  RGB), 2);
 Vprime     = sum(bsxfun(@times, Vp, RGB), 2);
+
+% Replace negative values with zero
+Scone(Scone<0) = 0;
+Vmaclamda(Vmaclamda<0) = 0;
+Melanopsin(Melanopsin<0) = 0;
+Vprime(Vprime<0) = 0;
 
 Temp = Melanopsin;
 idx  = Scone > C(3)*Vmaclamda;
